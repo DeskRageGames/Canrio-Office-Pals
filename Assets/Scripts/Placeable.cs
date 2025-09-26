b328efd5f1793c8c426ac549e217a229a6236d68
Meemitone <99894591+Meemitone@users.noreply.github.com>
2025-09-26 20:45:11 +0100

    Placeable script start

---
+++ Assets/Scripts/Placeable.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Placeable : MonoBehaviour
+{
+    [SerializeField] int xTileSize = 1, zTileSize = 1;
+    [SerializeField] GameObject noPlace, yesPlace, placedPlace;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
