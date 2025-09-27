cdb62a70512de78235b79dba565d0f8541aa480e
DeeAich <donnachadavylohan@gmail.com>
2025-09-27 10:53:14 +0100

    Monkey's can be productive

--- Assets/Scripts/Monkey/MonkeyStats.cs
+++ Assets/Scripts/Monkey/MonkeyStats.cs
@@ -4,15 +4,92 @@ using UnityEngine;
 
 public class MonkeyStats : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    public int level = 1;
+    public float prodPerSecond = 4;
+    public float prodGrowth = 1.5f;
+    public float stressPerSecond = 7.5f;
+    public float idleStressLower = 10f;
+    public float currentStress = 0;
+    [SerializeField] float maxStress = 100;
+
+    [SerializeField] bool idle = false;
+    [SerializeField] bool testing = false;
+
+    public void UpgradeMonkey()
     {
-        
+
+        level++;
+
+        prodPerSecond *= prodGrowth;
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void StressChange(float stressRelief, bool percentage = false)
     {
-        
+
+        stressPerSecond += percentage ? stressRelief * stressPerSecond : stressRelief;
+
     }
+
+    public float WorkMonkey(int tickCount)
+    {
+        //add up production over time
+        float generatedProd = prodPerSecond / tickCount;
+
+        float stressPercent = 1 - (currentStress / maxStress);
+        if (stressPercent < 0.1f)
+            stressPercent = 0.1f;
+
+        //manage Stress While working
+        currentStress += stressPerSecond / tickCount;
+        if (currentStress < 0)
+            currentStress = 0;
+        else if (currentStress > maxStress)
+            currentStress = maxStress;
+
+
+        return generatedProd * stressPercent;
+
+    }
+
+    public void IdleMonkey(int tickCount)
+    {
+
+        if(currentStress > 0)
+            currentStress -= idleStressLower / tickCount;
+        else
+            currentStress = 0;
+
+    }
+    /*
+    float testTimer;
+    float testProduction;
+
+    private void Update()
+    {
+
+        if (!testing)
+            return;
+
+        testTimer += Time.deltaTime;
+
+        if (testTimer > 0.25)
+        {
+
+            testTimer = 0;
+
+            if (idle)
+                IdleMonkey(4);
+            else
+            {
+                print (WorkMonkey(4));
+            }
+
+        }
+
+    }
+    */
+
 }
